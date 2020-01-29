5fd66842cd56835ce061f89ca15845a6559094a6
System Administrator <root@MacBook-Pro-de-Gilbert.local>
2020-01-29 11:20:16 -0500

    improved project structure

---
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Cockpit.css";
+const cockpit = props => {
+  let btnClass = "";
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+  const assignedClasses = [];
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm a React App</h1>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
+};
+
+export default cockpit;
