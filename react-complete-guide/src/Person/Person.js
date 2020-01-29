8b985a5dde6fb60a8d43eed3c9e9082335ad56e3
Gilbert Morett <gilbertmorett@MacBook-Pro-de-Gilbert.local>
2020-01-29 09:53:44 -0500

    removed error trow logic and added gitnore

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Person.css';
+import classes from "./Person.css";
 
-const person = ( props ) => {
-    const rnd = Math.random();
+const person = props => {
+  // used for displaying test error
+  // const rnd = Math.random();
 
-    if ( rnd > 0.7 ) {
-        throw new Error( 'Something went wrong' );
-    }
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+  // if ( rnd > 0.7 ) {
+  //     throw new Error( 'Something went wrong' );
+  // }
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
