2a3d2e1d2bacb073b091086afade0ff1702d4cc0
toni783 <toni7858@gmail.com>
2020-04-04 15:42:44 -0500

    addded logo and prettier config on burguer builder basic

--- burger-builder-basic/src/components/Layout/Layout.js
+++ burger-builder-basic/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => {
   return (
     <Fragment>
       <div>Toolbar ,sidedrawer ,backdrop </div>
+      <Toolbar/>
       <main className={classes.Content}>{props.children}</main>
     </Fragment>
   );
---
+++ burger-builder-basic/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navigationItems = () => {
+    return (
+        <ul>
+            <li>
+                {' '}
+                <a href="/">A Link</a>
+            </li>
+        </ul>
+    )
+}
+
+export default navigationItems
